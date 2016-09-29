5af5886faf1d0ef15e88ead797aa8bc983872670
Erik J. Sturcke <esturcke@cisco.com>
2016-09-28 23:46:54 -0400

    Define tests that fail only when generating coverage

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React        from 'react'
+import ReactDOM     from 'react-dom'
+import { mount }    from 'enzyme'
+import { Foo, Bar } from './App'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-});
+describe("<Foo>", () => {
+  it("renders 'Foo'", () => {
+    const wrapper = mount(<Foo/>)
+    console.log(wrapper.debug())
+    expect(wrapper.find("Foo").text()).toBe("Foo")
+  })
+})
+
+describe("<Bar>", () => {
+  it("renders 'Bar'", () => {
+    const wrapper = mount(<Bar/>)
+    console.log(wrapper.debug())
+    expect(wrapper.find("Bar").text()).toBe("Bar")
+  })
+})
